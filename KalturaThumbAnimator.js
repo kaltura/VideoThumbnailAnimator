7d2de6cce5b8a64b47584edab5f35a9b5c4e5d68
Zohar Babin <z.babin@gmail.com>
2019-01-07 14:10:12 +0200

    added startFrame override

--- KalturaThumbAnimator.js
+++ KalturaThumbAnimator.js
@@ -3,13 +3,15 @@ function KalturaThumbAnimator () {
   var _this = this;
   var lazyQuality = 65;
   var lazyWidth = 40;
+  var _startFrame = 0;
   
-  this.setup = function (thumbClassName, kalturaDomain = '://cfvod.kaltura.com', useLazy = false, lazyFilter = 'blur(3px) grayscale(100%) brightness(130%)') {
+  this.setup = function (thumbClassName, kalturaDomain = '://cfvod.kaltura.com', startFrame = 0, useLazy = false, lazyFilter = 'blur(3px) grayscale(100%) brightness(130%)') {
     var matches = Array.from(document.getElementsByClassName(thumbClassName));
     var _useLazy = useLazy;
+    _this._startFrame = startFrame;
     matches.forEach(function(thumbEl) {
       thumbData = _this.getKThumbData(thumbEl);
-      thumbEl.setAttribute('knextslice', 0);
+      thumbEl.setAttribute('knextslice', _this._startFrame);
       thumbEl.setAttribute('ktimer', 0);
       var thumbUniqueId = thumbData.entryId + '-' + (new Date().getUTCMilliseconds());
       thumbEl.setAttribute('kid', thumbUniqueId);
@@ -20,7 +22,7 @@ function KalturaThumbAnimator () {
       thumbEl.addEventListener("mouseup", _this.mouseOut);
       bgThumbSpriteUrl = kalturaDomain + '/p/'+thumbData.pid+'/thumbnail/entry_id/'+thumbData.entryId+'/width/'+thumbData.pxWidth+'/vid_slices/'+thumbData.spriteSlices+'/quality/'+thumbData.quality+'/type/'+thumbData.cropType+'/file_name/thumbnail.jpg';
       if (_useLazy) {
-        bgThumbSpriteUrlLowRes = kalturaDomain + '/p/'+thumbData.pid+'/thumbnail/entry_id/'+thumbData.entryId+'/width/'+(lazyWidth > 0 ? lazyWidth : thumbData.pxWidth)+'/quality/'+lazyQuality+'/type/'+thumbData.cropType+'/vid_slices/'+thumbData.spriteSlices+'/vid_slice/0/file_name/thumbnail.jpg';
+        bgThumbSpriteUrlLowRes = kalturaDomain + '/p/'+thumbData.pid+'/thumbnail/entry_id/'+thumbData.entryId+'/width/'+(lazyWidth > 0 ? lazyWidth : thumbData.pxWidth)+'/quality/'+lazyQuality+'/type/'+thumbData.cropType+'/vid_slices/'+thumbData.spriteSlices+'/vid_slice/'+_this._startFrame+'/file_name/thumbnail.jpg';
         thumbEl.style.backgroundImage = "url('" + bgThumbSpriteUrlLowRes + "')";
         thumbEl.style.backgroundPosition = "0% 0%";
         thumbEl.style.backgroundSize = "100% 100%";
@@ -35,7 +37,8 @@ function KalturaThumbAnimator () {
       else {
         thumbEl.style.backgroundImage = "url('" + bgThumbSpriteUrl + "')";
         thumbBgSizePercentage = thumbData.spriteSlices * 100; //total slices multiplied by 100%
-        thumbEl.style.backgroundPosition = "0% 0%";
+        startFramePos = _this._startFrame * (100 / (thumbData.spriteSlices - 1));
+        thumbEl.style.backgroundPosition = startFramePos+"% 0%";
         thumbEl.style.backgroundSize = thumbBgSizePercentage + "% 100%";
       }
     });
@@ -56,7 +59,8 @@ function KalturaThumbAnimator () {
     thumbEl.style.backgroundImage = "url('" + lazyimg.getAttribute('originalBgImageUrl') + "')";
     var totalSlices = parseInt(lazyimg.getAttribute('originalspriteSlices'));
     thumbBgSizePercentage = totalSlices * 100; //total slices multiplied by 100%
-    thumbEl.style.backgroundPosition = "0% 0%";
+    startFramePos = _this._startFrame * (100 / (totalSlices - 1));
+    thumbEl.style.backgroundPosition = startFramePos+"% 0%";
     thumbEl.style.backgroundSize = thumbBgSizePercentage + "% 100%";
     _this.applyFilter(thumbEl, "none");
   }
@@ -79,7 +83,7 @@ function KalturaThumbAnimator () {
     thumbData.keeprunning = parseInt(thumbEl.getAttribute('kislooping'));
     thumbData.keeprunning = (isNaN(thumbData.keeprunning) ? 0 : thumbData.keeprunning);
     thumbData.currentSlice = parseInt(thumbEl.getAttribute('knextslice'));
-    thumbData.currentSlice = (isNaN(thumbData.currentSlice) ? 0 : thumbData.currentSlice);
+    thumbData.currentSlice = (isNaN(thumbData.currentSlice) ? _this._startFrame : thumbData.currentSlice);
     thumbData.kid = thumbEl.getAttribute('kid');
     return thumbData;
   }
@@ -94,7 +98,7 @@ function KalturaThumbAnimator () {
       thumbData = _this.getKThumbData(vidthumb);
       if (thumbData.keeprunning > 0) {
           var nextSlice = thumbData.currentSlice + 1;
-          if (nextSlice == thumbData.spriteSlices) nextSlice = 0;
+          if (nextSlice == thumbData.spriteSlices) nextSlice = _this._startFrame;
           vidthumb.setAttribute('knextslice', nextSlice);
           var cSlicePercentagePosX = thumbData.currentSlice * (100 / (thumbData.spriteSlices - 1));
           vidthumb.style.backgroundPosition = cSlicePercentagePosX + "% 0%";
@@ -108,9 +112,11 @@ function KalturaThumbAnimator () {
 
   this.mouseOut = function (e) {
       var vidthumb = e.target;
+      thumbData = _this.getKThumbData(vidthumb);
       vidthumb.setAttribute('kislooping', 0);
-      vidthumb.style.backgroundPosition = "0% 0%";
-      vidthumb.setAttribute('knextslice', 0);
+      startFramePos = _this._startFrame * (100 / (thumbData.spriteSlices - 1));
+      vidthumb.style.backgroundPosition = startFramePos+"% 0%";
+      vidthumb.setAttribute('knextslice', _this._startFrame);
       vidthumb.setAttribute('ktimer', 0);
   }
 }
